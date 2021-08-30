02ceb3aea9620553b4b587453ed520a0fa5927b8
Kamil Krawczak <Kamil.Krawczak@onet.eu>
2021-08-30 18:45:25 +0200

    End modification js

--- script/script.js
+++ script/script.js
@@ -9,6 +9,7 @@ const secondPlayerPoints = document.querySelector('.second-player p');
 const photo = document.getElementById('photo');
 const textFirst = document.getElementById('text-first');
 const textSecond = document.getElementById('text-second');
+const btn = document.querySelector('.btn');
 
 let Score = {
   firstPlayer: 0,
@@ -25,9 +26,11 @@ function whoWon() {
   if (Score.firstPlayer > Score.secondPlayer) {
     firstPlayer.classList.add('win');
     textFirst.textContent = 'Wygrywasz!!!🎉🎉🎉';
+    firstPlayerPoints.textContent = ++Points.firstPlayer;
   } else if (Score.secondPlayer > Score.firstPlayer) {
     secondPlayer.classList.add('win');
     textSecond.textContent = 'Wygrywasz!!!🎉🎉🎉';
+    secondPlayerPoints.textContent = ++Points.secondPlayer;
   } else {
     firstPlayer.classList.add('win');
     secondPlayer.classList.add('win');
@@ -36,17 +39,27 @@ function whoWon() {
   }
   photo.src = 'img/tył.jpg';
 }
+function refresh() {
+  Score.firstPlayer = 0;
+  Score.secondPlayer = 0;
+  flag = true;
+  click = 0;
+  firstPlayer.classList.remove('win');
+  secondPlayer.classList.remove('win');
+  firstPlayerScore.textContent = 0;
+  secondPlayerScore.textContent = 0;
+  textFirst.textContent = '';
+  textSecond.textContent = '';
+}
 
 function addPoints(point) {
   if (flag) {
     firstPlayer.classList.add('active');
-    Points.firstPlayer += point;
-    Score.firstPlayer = Points.firstPlayer;
-    firstPlayerPoints.textContent = Points.firstPlayer;
+    Score.firstPlayer += point;
+    firstPlayerScore.textContent = Score.firstPlayer;
   } else {
-    Points.secondPlayer += point;
-    Score.secondPlayer = Points.secondPlayer;
-    secondPlayerPoints.textContent = Points.secondPlayer;
+    Score.secondPlayer += point;
+    secondPlayerScore.textContent = Score.secondPlayer;
   }
 }
 
@@ -61,7 +74,6 @@ function StartGame() {
 function playerChange() {
   click++;
   if (click <= 1) {
-    firstPlayerPoints.textContent = 0;
     firstPlayerScore.textContent =
       Score.firstPlayer <= 21 ? Score.firstPlayer : (Score.firstPlayer = 0);
     flag = !flag;
@@ -70,11 +82,11 @@ function playerChange() {
       secondPlayer.classList.add('active');
     }
   } else {
-    secondPlayer.classList.remove('active');
-    secondPlayerPoints.textContent = 0;
     secondPlayerScore.textContent =
       Score.secondPlayer <= 21 ? Score.secondPlayer : (Score.secondPlayer = 0);
+    secondPlayer.classList.remove('active');
     whoWon();
+    setTimeout(refresh, 2000);
   }
 }
 
